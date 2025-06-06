66fa15cb63a188006c38f092bf6f9a01ec23ed57
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2025-06-06 17:27:15 +0300

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Categories } from './components/categories/categories';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./home/home').then((m) => m.Home)
+
+    },
+    {
+        path: 'category/:id',
+        component: Categories
+    }
+];
