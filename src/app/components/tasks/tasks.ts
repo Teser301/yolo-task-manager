a36dc59a2c2b79c9007be6b4b3e6f5fd1eddbd87
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2025-06-06 17:25:19 +0300

    feat: tasks view

---
+++ src/app/components/tasks/tasks.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [RouterLink],
+  templateUrl: './tasks.html',
+  styleUrl: './tasks.scss'
+})
+export class Tasks {
+
+}
