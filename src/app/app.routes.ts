9d635fafd382e18ebb115d80b20d77f8a2f4b2ca
Teser301 <30408387+Teser301@users.noreply.github.com>
2025-06-07 22:33:30 +0300

    refactor: router cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
-import { Categories } from './components/categories/categories';
 
 export const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         loadComponent: () => import('./home/home').then((m) => m.Home)
-
     },
     {
-        path: 'category/:id',
-        component: Categories
+        path: 'category/:categoryId',
+        children: [
+            {
+                path: "",
+                loadComponent: () => import('./pages/category/category').then((m) => m.CategoryView)
+            },
+            {
+                path: "task/:taskId",
+                loadComponent: () => import('./pages/task/task').then((m) => m.TaskView)
+            }
+        ]
     }
 ];
