6a9c11c49c1711674cb986db28ab921bb750b29b
Teser301 <30408387+Teser301@users.noreply.github.com>
2025-06-07 22:33:45 +0300

    refactor: enable task array

--- src/app/services/category.spec.ts
+++ src/app/services/category.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { CategoryService } from './category';
 
-import { Category } from './category';
-
-describe('Category', () => {
-  let service: Category;
+describe('CategoryService', () => {
+  let service: CategoryService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Category);
+    service = TestBed.inject(CategoryService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all categories', () => {
+    const categories = service.getCategories();
+    expect(categories.length).toBe(2);
+  });
+
+  it('should return a category by ID', () => {
+    const category = service.getCategory(1);
+    expect(category?.title).toBe('Work');
+  });
+
+  it('should delete a category', () => {
+    const success = service.deleteCategory(1);
+    expect(success).toBeTrue();
+    expect(service.getCategories().length).toBe(1);
   });
 });
--- src/app/services/category.ts
+++ src/app/services/category.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../models/category.model';
-
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
   private categories: Category[] = [
-    { id: 1, title: 'Work' },
-    { id: 2, title: 'Hobbies' },
+    {
+      id: 1,
+      title: 'Work',
+      description: 'For work things.',
+      tasks: [
+        {
+          id: 1,
+          title: 'Call Mechanic',
+          status: 'To Do',
+          date: new Date('2025-06-13')
+        },
+        {
+          id: 2,
+          title: 'Attend Meeting',
+          status: 'In Progress',
+          date: new Date('2025-06-13'),
+          description: 'Quarterly planning'
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Hobbies',
+      tasks: []
+    }
   ];
   constructor() { }
+
   getCategories(): Category[] {
     return this.categories;
   }
   getCategory(id: number): Category | undefined {
     return this.categories.find(category => category.id === id);
   }
+
   deleteCategory(id: number): boolean {
     const initialLength = this.categories.length;
     this.categories = this.categories.filter(category => category.id !== id);
     return this.categories.length !== initialLength;
+  }
+
+  deleteTask(categoryId: number, taskId: number): void {
+    const category = this.categories.find(c => c.id === categoryId);
+    if (category) {
+      category.tasks = category.tasks.filter(task => task.id !== taskId);
+    }
   }
 }
