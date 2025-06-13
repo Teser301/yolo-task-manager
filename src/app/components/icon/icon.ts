33c0c0281c008e609ef97305a056317fc2d52646
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2025-06-13 09:34:55 +0300

    feat: icon component

---
+++ src/app/components/icon/icon.ts
@@ -0,0 +1,28 @@
+import { Component, input } from '@angular/core';
+
+export type IconType = 'calendar' | 'view' | 'edit' | 'delete' | 'status';
+
+@Component({
+  selector: 'app-icon',
+  imports: [],
+  templateUrl: './icon.html',
+  styleUrl: './icon.scss'
+})
+export class Icon {
+  type = input.required<IconType>();
+
+  get iconPath(): string {
+    switch (this.type()) {
+      case 'calendar':
+        return 'M19 3h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z';
+      case 'view':
+        return 'M12 4.5C7 4.5 2.7 7.6 1 12c1.7 4.4 6 7.5 11 7.5s9.3-3.1 11-7.5c-1.7-4.4-6-7.5-11-7.5zM12 17c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5zm0-8c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3z';
+      case 'edit':
+        return 'M3 17.2V21h3.8L17.8 9.9l-3.8-3.8L3 17.2zM20.7 7c.4-.4.4-1 0-1.4l-2.3-2.3c-.4-.4-1-.4-1.4 0l-1.8 1.8 3.8 3.8 1.7-1.9z';
+      case 'delete':
+        return 'M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z';
+      default:
+        return '';
+    }
+  }
+}
